200f640754de61a69575dccb732acbb74db856d7
McShaman <timmclaren@mcshaman.com>
2021-07-04 10:12:09 +1000

    ✨ Send report command

---
+++ send-report.js
@@ -0,0 +1,51 @@
+import meow from 'meow'
+import sendReport from './lib/sendReport.js'
+
+const cli = meow(`
+	Usage
+	  $ send-report
+	
+	Options
+	  --from       Senders email address. Can be plain 'sender@server.com' or formatted '"Sender Name" sender@server.com'.
+
+	  --smtp-host  SMTP server hostname or IP address. If any of the SMTP options are not defined test SMTP credentials will be used. 
+
+	  --smtp-pass  SMTP user password. If any of the SMTP options are not defined test SMTP credentials will be used.
+
+	  --smtp-port  SMTP server port. If any of the SMTP options are not defined test SMTP credentials will be used.
+
+	  --smtp-user  SMTP user name. If any of the SMTP options are not defined test SMTP credentials will be used.
+
+	  --to         Comma separated list or an array of recipients email addresses.
+`, {
+	importMeta: import.meta,
+	allowUnknownFlags: false,
+	booleanDefault: undefined,
+	flags: {
+		from: {
+			type: 'string',
+			isRequired: true,
+		},
+		to: {
+			type: 'string',
+			isRequired: true,
+		},
+		smtpHost: {
+			type: 'string',
+		},
+		smtpPort: {
+			type: 'number',
+		},
+		smtpUser: {
+			type: 'string',
+		},
+		smtpPass: {
+			type: 'string',
+		},
+		help: {
+			type: 'boolean',
+		},
+	},
+})
+
+sendReport(cli.flags)

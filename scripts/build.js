ad55d7fdb95d0e2522b454d72b8a61def4b22bf5
McShaman <timmclaren@mcshaman.com>
2021-09-01 21:27:37 +1000

    🖍  Build Docker image script

---
+++ scripts/build.js
@@ -0,0 +1,19 @@
+import childProcess from 'child_process'
+import fs from 'fs'
+import path from 'path'
+
+childProcess.execSync('npm pack', { stdio: 'inherit' })
+
+const file = fs.readFileSync(path.join(process.cwd(), 'package.json'))
+const { name, version } = JSON.parse(file.toString())
+
+const packFile = `${name}-${version}.tgz`
+
+childProcess.execSync(`
+	docker build \
+		--build-arg PACK_FILE="${packFile}" \
+		--tag ${name}:${version} \
+		.
+`, { stdio: 'inherit' })
+
+fs.unlinkSync(path.join(process.cwd(), packFile))
